3b40bb1f19699551b3f3561da001a5b5a8b6c3e5
ChelpanovPA <pavel.chelpanov@mail.ru>
2022-12-08 12:11:03 +0500

    Добавил минимальные комментраии к классам.

--- new_task1/ClassHierarchy.cs
+++ new_task1/ClassHierarchy.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 
+/*
+ * Использовал статью https://habr.com/ru/sandbox/152884/ в качестве примера
+ */
+
 namespace new_task1
 {
     public abstract class Transport //базовый класс для всех остальных, не предполагаю, что он будет создан сам по себе
@@ -8,32 +12,32 @@ namespace new_task1
         public int Year;
         public int Weight;
 
-        protected Transport()
+        protected Transport() //Констурктор при неизвестных входных данных
         {
             Color = "Unknown";
             Year = 0;
             Weight = 0;
         }
 
-        protected Transport(string color, int year, int weight)
+        protected Transport(string color, int year, int weight) //Конструктор с известными входными данными
         {
             Color = color;
             Year = year;
             Weight = weight;
         }
-        public abstract void Information();
+        public abstract void Information(); //Метод вывода на экран информация, дополнительно переопределю ToString()
 
     }
 
-    class Train : Transport
+    class Train : Transport //Поезд, наследую от транспорта
     {
-        public int Сarriages;
-        public Train(string color, int year, int weight, int carriages) : base(color, year, weight)
+        public int Сarriages; //Количество вагонов состава
+        public Train(string color, int year, int weight, int carriages) : base(color, year, weight) //Конструктор
         {
             Сarriages = carriages;
         }
 
-        public override void Information()
+        public override void Information() //Переопределяю метод
         {
             Console.WriteLine("Train");
             Console.WriteLine($"Year: {base.Year}\n" +
@@ -43,10 +47,10 @@ namespace new_task1
         }
     }
 
-    class Car : Transport
+    class Car : Transport //Автомобиль, наследую от транспорта
     {
-        public string Mark;
-        public Car(string color, int year, int weight, string mark) : base(color, year, weight)
+        public string Mark; //Маорка автомобиль
+        public Car(string color, int year, int weight, string mark) : base(color, year, weight) //Констурктор
         {
             Mark = mark;
         }
@@ -61,15 +65,15 @@ namespace new_task1
         }
     }
 
-    class Truck : Car 
+    class Truck : Car //Грузовик, наследую от автомаобиля
     { 
-        public int LoadCapacity;
-        public Truck(string color, int year, int weight, string mark, int loadCapacity) : base(color, year, weight, mark)
+        public int LoadCapacity; //Загрузка грузовика
+        public Truck(string color, int year, int weight, string mark, int loadCapacity) : base(color, year, weight, mark) //Конструктор
         {
             LoadCapacity = loadCapacity;
         }
 
-        public override void Information()
+        public override void Information() //Переопределяю метод
         {
             Console.WriteLine("Truck Car");
             Console.WriteLine($"Mark: {Mark}\n"+
@@ -80,17 +84,17 @@ namespace new_task1
         }
     }
 
-    class Passenger : Car
+    class Passenger : Car //Легковой автомобиль
     {
-        public string PassengerType;
-        public Passenger(string color, int year, int weight, string mark, string passengerType) : base(color, year, weight, mark)
+        public string PassengerType; //Тип лекгового автомобиля
+        public Passenger(string color, int year, int weight, string mark, string passengerType) : base(color, year, weight, mark) //Конструктор
         {
             PassengerType = passengerType;
         }
 
-        public override void Information()
+        public override void Information() //Переопределяю метод
         {
-            Console.WriteLine("Passenger Car");
+            Console.WriteLine("Passenger Car"); 
             Console.WriteLine($"Mark: {Mark}\n" +
                               $"Year: {base.Year}\n" +
                               $"Weight: {base.Weight}\n" +
